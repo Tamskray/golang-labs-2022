503cad5349c55548e88c39c113bb0f1e3ea2432a
Vlad Kopylov <kn192_kvr@student.ztu.edu.ua>
2022-02-08 17:52:01 +0200

    Lab1

---
+++ Lab1/lab1/ex10.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var chartype int8 = 'R'
+
+	fmt.Printf("Code '%c' - %d\n", chartype, chartype)
+
+	//Задание.
+	//1. Вывести украинскую букву 'Ї'
+	//2. Пояснить назначение типа "rune"
+}
